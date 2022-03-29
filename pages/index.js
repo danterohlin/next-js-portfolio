6c6de276bf389bce859597be3986bb2a12a8a1ba
danterohlin <56160908+danterohlin@users.noreply.github.com>
2022-03-29 22:39:10 +0200

    m

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Index({ allGames, allPosts, preview }) {
 		<>
 			<Layout preview={preview}>
 				<Head>
-					<title>Next.js Blog Example with {CMS_NAME}</title>
+					<title>Andreas Portfolio</title>
 				</Head>
 				<Container>
 					<Intro games={allGames} />
